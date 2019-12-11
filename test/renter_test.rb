dd8a700194fbe6c76bfa8500f3467cef562d8b1a
Cassie Achzenick <caachzenick@gmail.com>
2019-12-11 09:27:00 -0700

    add passing renter test iteration 2

--- test/renter_test.rb
+++ test/renter_test.rb
@@ -3,4 +3,12 @@ require 'minitest/pride'
 require './lib/renter'
 
 class RenterTest < Minitest::Test
+
+  def setup
+    @renter1 = Renter.new("Jessie")
+  end
+
+  def test_it_has_a_name
+    assert_equal "Jessie", @renter1.name
+  end
 end
