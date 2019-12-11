6f5756cb81192b3713259e09c13401d026866efe
Cassie Achzenick <caachzenick@gmail.com>
2019-12-11 09:27:36 -0700

    add passing add unit method

--- lib/building.rb
+++ lib/building.rb
@@ -1,2 +1,16 @@
 class Building
+
+  attr_reader :units
+
+  def initialize
+    @units = []
+  end
+
+  def add_unit(unit)
+    @units << unit
+  end
+
+  def average_rent
+    @units.inject(0) { |sum, unit| require "pry"; binding.pry}
+  end
 end
